0bc14ae382f4737f799f4e12141246d51388d357
hideo55 <hide.o.j55@gmail.com>
2012-07-03 22:33:40 +0900

    initial commit

---
+++ lib/ArangoDB.pm
@@ -0,0 +1,32 @@
+package ArangoDB;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ArangoDB -
+
+=head1 SYNOPSIS
+
+  use ArangoDB;
+
+=head1 DESCRIPTION
+
+ArangoDB is
+
+=head1 AUTHOR
+
+Hideaki Ohno E<lt>hide.o.j55 {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
